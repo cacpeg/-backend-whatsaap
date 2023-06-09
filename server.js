b5b0bec4509e689a0f8d9a25859f7b7699f37972
Jefferson Alvarez <admin@cacpeg.fn.ec>
2023-06-09 01:47:37 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+import { createBot } from 'whatsapp-cloud-api';
+import cors from 'cors';
+import express from 'express';
+// or if using require:
+// const { createBot } = require('whatsapp-cloud-api');
+const bot = createBot();
+
+const app = express()
+
+
+
+await bot.startExpressServer({
+  webhookVerifyToken: 'my-verification-token',
+  port: 3000,
+  webhookPath: `/custom/webhook`,  
+  useMiddleware: (app) => {
+    app.use(cors()),
+    app.get("/apis",(req,res)=>{
+      bot.sendText("","")
+    })
+  },
+});
